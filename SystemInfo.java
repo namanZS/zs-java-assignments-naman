fcc3c14890f09ba3360c0af1b6883879aa61c2eb
naman-zs <naman.gupta@zopsmart.com>
2024-01-10 17:10:32 +0530

    Assignment-1

---
+++ SystemInfo.java
@@ -0,0 +1,46 @@
+
+
+import java.io.File;
+
+public class SystemInfo {
+
+    public static void main(String[] args) {
+        printCurrentUser();
+        printHomeDirectory();
+        printSystemMemoryInfo();
+        printSystemCPUInfo();
+        printSystemDiskInfo();
+        printOSInfo();
+    }
+    private static void printCurrentUser(){
+        System.out.println("Current User: " + System.getProperty("user.name"));
+
+    }
+    private static void printHomeDirectory() {
+
+        System.out.println("Home Directory: " + System.getProperty("user.home"));
+    }
+
+    private static void printSystemMemoryInfo() {
+        Runtime runtime = Runtime.getRuntime();
+        long totalMemory = runtime.totalMemory();
+        System.out.println("System Memory:");
+        System.out.println("  Total Memory: " + totalMemory/(512*1024) +" GB");
+    }
+
+    private static void printSystemCPUInfo() {
+        int count = Runtime.getRuntime().availableProcessors();
+        System.out.println("System CPU/Cores: " + count);
+    }
+
+    private static void printSystemDiskInfo() {
+        File root = new File("/");
+        long totalDiskSpace = root.getTotalSpace();
+        System.out.println("System Disk Size: " + totalDiskSpace / (1024 * 1024 * 1024) + " GB");
+    }
+
+    private static void printOSInfo() {
+        System.out.println("OS Build: " + System.getProperty("os.arch"));
+        System.out.println("OS Version: " + System.getProperty("os.version"));
+    }
+}
---
+++ out/production/com.zs/readme.md
@@ -0,0 +1,1 @@
+Hi there this repo is for assignments submission from NAMAN GUPTA
