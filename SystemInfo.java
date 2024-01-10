3d426b0efc8b740066afe40c8c52e56080c04c47
naman-zs <naman.gupta@zopsmart.com>
2024-01-10 17:38:32 +0530

    Revert "rectified error in assignment 1"
    This reverts commit 0df64aa67fa5fd7188ffcb71172e7e647e75ddb0.

--- SystemInfo.java
+++ SystemInfo.java
@@ -1,4 +1,5 @@
 
+
 import java.io.File;
 
 public class SystemInfo {
@@ -13,15 +14,18 @@ public class SystemInfo {
     }
     private static void printCurrentUser(){
         System.out.println("Current User: " + System.getProperty("user.name"));
+
     }
     private static void printHomeDirectory() {
+
         System.out.println("Home Directory: " + System.getProperty("user.home"));
     }
 
     private static void printSystemMemoryInfo() {
         Runtime runtime = Runtime.getRuntime();
-        long maxmemory = runtime.maxMemory();
-        System.out.println("Total Memory: " + maxmemory/(8*1024*1024) +" GB");
+        long totalMemory = runtime.totalMemory();
+        System.out.println("System Memory:");
+        System.out.println("  Total Memory: " + totalMemory/(512*1024) +" GB");
     }
 
     private static void printSystemCPUInfo() {
